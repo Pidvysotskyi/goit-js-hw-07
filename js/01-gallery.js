e7a6c3cc25423301c2c58646fd8d47c05da6361e
Oleksandr <Pidvysotskyi.oleksandr@gmail.com>
2022-07-18 20:51:09 +0300

    markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galeryMarkup = galleryItems.map(({ original, preview, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+  </a>
+  </div>`;
+});
+
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.insertAdjacentHTML("afterbegin", galeryMarkup.join(""));
+
+galleryRef.addEventListener("click", onGalerryClick);
+
+function onGalerryClick(event) {
+  event.preventDefault();
+  const imageUrl = event.target.dataset.source;
+
+  return imageUrl;
+}
